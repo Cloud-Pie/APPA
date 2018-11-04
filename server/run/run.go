f088cc0d53ff0201c47116b1e4def3c24895f7fc
ansjin <anshuljindal.nith@gmail.com>
2018-11-04 20:07:36 +0100

    updating basic server logging

--- server/run/run.go
+++ server/run/run.go
@@ -7,6 +7,6 @@ import (
 
 func RunServer()  {
 	router := NewRouter()
-	log.Fatal(http.ListenAndServe(":8080", router))
-	log.Fatal("Server started on port 8080")
+	log.Println(http.ListenAndServe(":8080", router))
+	log.Println("Server started on port 8080")
 }
