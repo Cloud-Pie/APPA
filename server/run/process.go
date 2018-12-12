014e09fe9a49af5fea31e4ec9108651ab4005fc7
ansjin <anshuljindal.nith@gmail.com>
2018-12-12 15:53:35 +0100

    update instance start script

--- server/run/process.go
+++ server/run/process.go
@@ -132,6 +132,9 @@ region= `+AWSConfig.Region+`
 output=json
 EOM
 git clone `+ gitPath+ `
+aws s3 cp s3://boundarydata/Inlet_Data.zip Inlet_Data.zip
+tar -xzvf Inlet_Data.zip
+cp -R Inlet_Data\Inlet_Data openfoam\openfoam_src\example
 cd openfoam/scripts
 sh ./deploy_app.sh
 $file_name = /results/result.tar.gz 
